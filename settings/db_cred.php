25e894d3bc05c3828a8911b8b79d84725393c508
Mike Ofori <michael.ofori@ashesi.edu.gh>
2022-10-21 21:22:10 +0000

    db server

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -2,10 +2,10 @@
 //Database credentials
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'class_contact_mgt');
+define('DB_SERVER', 'us-cdbr-east-06.cleardb.net');
+define('DB_USERNAME', 'b5880261eaf58f');
+define('DB_PASSWORD', '7039c06d');
+define('DB_NAME', 'heroku_be8205cf6fda2b0');
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -16,3 +16,6 @@ if($conn === false){
 }
 
 ?>
+
+
+mysql://b5880261eaf58f:7039c06d@us-cdbr-east-06.cleardb.net/heroku_be8205cf6fda2b0?reconnect=true
