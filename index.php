55ae1b46facb0197fb1940e2306f83f85c3f85a9
Mike Ofori <michael.ofori@ashesi.edu.gh>
2022-10-21 21:24:47 +0000

    w

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-<?php
-	// landing/index page
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Labs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-<?php
-    echo "Hello World server";
-?>
+<div class="container">
+  <h2>Contact Management System</h2>
+  <a href="view/form.php" class="btn btn-info" role="button">Add contact</a>
+  <!-- <a href="login/login.php" class="btn btn-info" role="button">Lab 2</a> -->
+  <!-- <button type="button" class="btn btn-info">Button</button>
+  <input type="button" class="btn btn-info" value="Input Button">
+  <input type="submit" class="btn btn-info" value="Submit Button"> -->
+</div>
+
+</body>
+</html>
