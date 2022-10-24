5ccb1feccd226a3bf6629d6385d4f8d34b4627b5
Mike Ofori <michael.ofori@ashesi.edu.gh>
2022-10-24 15:18:48 +0000

    ddc

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -8,8 +8,9 @@ define('DB_PASSWORD', '7039c06d');
 define('DB_NAME', 'heroku_be8205cf6fda2b0');
  
 /* Attempt to connect to MySQL database */
+
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+mysqli_set_charset ($conn,"utf8");
 // Check connection
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
